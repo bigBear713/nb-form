126d0c5906d188ab8d256f146f8fe371e20cd91e
bigBear713 <774326739@qq.com>
2022-03-12 10:41:30 +0800

    UPDATE: project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nb-form'`, () => {
+  it(`should have as title 'nb-form-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nb-form');
+    expect(app.title).toEqual('nb-form-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nb-form app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('nb-form-demo app is running!');
   });
 });

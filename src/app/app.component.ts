305a9f696dee0439b208cb81c937c39cac2634ac
bigBear713 <774326739@qq.com>
2022-03-13 00:31:12 +0800

    ADD: demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NbTransLangEnum, NbTransService } from '@bigbear713/nb-trans';
+import { NbControlErrTypeEnum, NbFormService, NbFormValidators } from 'nb-form';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nb-form-demo';
+export class AppComponent implements OnInit {
+
+  form: FormGroup | undefined;
+
+  errMapping1 = {
+    [NbControlErrTypeEnum.REQUIRED]: this.transService.translationAsync('required'),
+  };
+
+  errMapping2 = {
+    [NbControlErrTypeEnum.FILE_MAX_SIZE]: 'The file max file is 100kb!',
+  };
+
+  get field1Ctrl(): FormControl {
+    return this.form?.get('field1') as FormControl;
+  }
+
+  get field2Ctrl(): FormControl {
+    return this.form?.get('field2') as FormControl;
+  }
+
+  get field3Ctrl(): FormControl {
+    return this.form?.get('field3') as FormControl;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private formService: NbFormService,
+    private transService: NbTransService,
+  ) { }
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  changeLanguage(langKey: string) {
+    this.transService.changeLangSync(
+      langKey === 'en' ? NbTransLangEnum.EN : NbTransLangEnum.ZH_CN
+    );
+  }
+
+  onChangeFile($event: Event): void {
+    const fileEle = $event.target as HTMLInputElement;
+    if (fileEle && fileEle.files?.length) {
+      this.field3Ctrl.setValue(fileEle.files[0]);
+      this.field3Ctrl.markAsTouched();
+    }
+  }
+
+  private buildForm(): void {
+    this.form = this.fb.group({
+      field1: [null, [NbFormValidators.required(true)]],
+      field2: [null, this.formService.getValidatorsFromControlConfig({ whitespace: false })],
+      field3: [undefined, [
+        NbFormValidators.fileType(['image/svg+xml', 'image/jpeg']),
+        NbFormValidators.fileSize({ maxSize: 500 * 1000, minSize: 100 * 1000 }),
+      ]],
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NbTransLangEnum, NbTransModule, NbTransService, NB_TRANS_LOADER } from '@bigbear713/nb-trans';
+import {
+  NbFormModule,
+  NbControlErrTypeEnum,
+  NB_CONTROL_COMMON_ERR_MAPPING_TOKEN
+} from 'nb-form';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +17,35 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NbFormModule,
+    NbTransModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: NB_CONTROL_COMMON_ERR_MAPPING_TOKEN,
+      useFactory: (transService: NbTransService) => ({
+        [NbControlErrTypeEnum.FILE_TYPE]: transService.translationAsync('fileType'),
+        [NbControlErrTypeEnum.FILE_MIN_SIZE]: 'The file min file is 50KB!',
+      }),
+      deps: [NbTransService]
+    },
+    {
+      provide: NB_TRANS_LOADER,
+      useValue: {
+        [NbTransLangEnum.ZH_CN]: {
+          required: '这个字段为必填项！',
+          fileType: '文件格式要求为jpg/svg!'
+        },
+        [NbTransLangEnum.EN]: {
+          required: 'This field is required!',
+          fileType: 'The file format should be jpg/svg!'
+        },
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

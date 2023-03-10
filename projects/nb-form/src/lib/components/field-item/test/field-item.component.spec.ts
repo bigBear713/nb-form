f803e1296aad9ba82fa0f83494ec73f4f4194733
bigBear713 <774326739@qq.com>
2023-03-10 22:50:37 +0800

    ut: add standalone component unit tests

--- projects/nb-form/src/lib/components/field-item/test/field-item.component.spec.ts
+++ projects/nb-form/src/lib/components/field-item/test/field-item.component.spec.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { UntypedFormControl } from '@angular/forms';
+import { FormControl, UntypedFormControl } from '@angular/forms';
+import { NbControlErrType } from '../../../constants';
 import { INbControlErrInfo } from '../../../models';
 import { NbFormTestingModule } from '../../../testing';
+import { NbFormValidators } from '../../../validators';
 import { NbFieldItemComponent } from '../field-item.component';
 
 @Component({
@@ -22,69 +24,120 @@ class UIComponent {
 }
 
 describe('NbFieldItemComponent', () => {
-  let component: NbFieldItemComponent;
-  let fixture: ComponentFixture<NbFieldItemComponent>;
+  describe('used in normal component', () => {
 
-  beforeEach(async () => {
-    TestBed.configureTestingModule({
-      imports: [NbFormTestingModule],
-      declarations: [UIComponent]
-    })
-      .compileComponents();
-  });
+    let component: NbFieldItemComponent;
+    let fixture: ComponentFixture<NbFieldItemComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NbFieldItemComponent);
-    component = fixture.componentInstance;
-    component.control = new UntypedFormControl();
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  describe('verify the UI', () => {
-    let uiFixture: ComponentFixture<UIComponent>;
-    let uiComponent: UIComponent;
-    let uiHostEle: HTMLElement;
+    beforeEach(async () => {
+      TestBed.configureTestingModule({
+        imports: [NbFormTestingModule],
+        declarations: [UIComponent]
+      })
+        .compileComponents();
+    });
 
     beforeEach(() => {
-      uiFixture = TestBed.createComponent(UIComponent);
-      uiComponent = uiFixture.componentInstance;
-      uiComponent.control = new UntypedFormControl();
-      uiFixture.detectChanges();
-      uiHostEle = uiFixture.debugElement.nativeElement;
+      fixture = TestBed.createComponent(NbFieldItemComponent);
+      component = fixture.componentInstance;
+      component.control = new UntypedFormControl();
+      fixture.detectChanges();
     });
 
-    it('verify the required icon', () => {
-      uiComponent.required = false;
-      uiFixture.detectChanges();
-      expect(uiHostEle.querySelector('.label-required')).toBeFalsy();
-
-      uiComponent.required = true;
-      uiFixture.detectChanges();
-      expect(uiHostEle.querySelector('.label-required')).toBeTruthy();
-
-      uiComponent.required = undefined;
-      uiFixture.detectChanges();
-      expect(uiHostEle.querySelector('.label-required')).toBeFalsy();
+    it('should create', () => {
+      expect(component).toBeTruthy();
     });
 
-    it('verify the field label content', () => {
-      const fieldLabelEle: HTMLElement | null = uiHostEle.querySelector('.field-label');
-      const labelNode: ChildNode | undefined = fieldLabelEle?.childNodes.item(2);
-      expect(labelNode?.textContent?.trim()).toEqual('fieldLabel');
+    describe('verify the UI', () => {
+      let uiFixture: ComponentFixture<UIComponent>;
+      let uiComponent: UIComponent;
+      let uiHostEle: HTMLElement;
+
+      beforeEach(() => {
+        uiFixture = TestBed.createComponent(UIComponent);
+        uiComponent = uiFixture.componentInstance;
+        uiComponent.control = new UntypedFormControl();
+        uiFixture.detectChanges();
+        uiHostEle = uiFixture.debugElement.nativeElement;
+      });
+
+      it('verify the required icon', () => {
+        uiComponent.required = false;
+        uiFixture.detectChanges();
+        expect(uiHostEle.querySelector('.label-required')).toBeFalsy();
+
+        uiComponent.required = true;
+        uiFixture.detectChanges();
+        expect(uiHostEle.querySelector('.label-required')).toBeTruthy();
+
+        uiComponent.required = undefined;
+        uiFixture.detectChanges();
+        expect(uiHostEle.querySelector('.label-required')).toBeFalsy();
+      });
+
+      it('verify the field label content', () => {
+        const fieldLabelEle: HTMLElement | null = uiHostEle.querySelector('.field-label');
+        const labelNode: ChildNode | undefined = fieldLabelEle?.childNodes.item(2);
+        expect(labelNode?.textContent?.trim()).toEqual('fieldLabel');
+      });
+
+      it('verify the field content', () => {
+        const fieldContentEle: HTMLElement | null = uiHostEle.querySelector('.field-content');
+        expect(fieldContentEle?.querySelector('input')).toBeTruthy();
+      })
+
+      it('verify the control error', () => {
+        const fieldContentEle: HTMLElement | null = uiHostEle.querySelector('.field-content');
+        expect(fieldContentEle?.querySelector('nb-control-err')).toBeTruthy();
+      })
     });
+  });
 
-    it('verify the field content', () => {
-      const fieldContentEle: HTMLElement | null = uiHostEle.querySelector('.field-content');
-      expect(fieldContentEle?.querySelector('input')).toBeTruthy();
-    })
+  describe('used in standalone component', () => {
+    [
+      {
+        title: 'imported by standalone component',
+        createComp: () => TestBed.createComponent(StandaloneComponent)
+      },
+      {
+        title: 'imported by ngModule',
+        createComp: () => TestBed.createComponent(StandaloneComponentWithNgModule)
+      }
+    ].forEach(item => {
+      it(item.title, () => {
+        const fixture = item.createComp();
+        const component = fixture.componentInstance;
+        component.control.markAsDirty();
+        fixture.detectChanges();
 
-    it('verify the control error', () => {
-      const fieldContentEle: HTMLElement | null = uiHostEle.querySelector('.field-content');
-      expect(fieldContentEle?.querySelector('nb-control-err')).toBeTruthy();
+        expect(component.textContent).toEqual('The field is required!');
+      });
     })
   });
 });
+
+
+
+const StandaloneCompConfig = {
+  standalone: true,
+  imports: [NbFieldItemComponent],
+  template: `<nb-field-item [control]="control" [errInfo]="errInfo"></nb-field-item>`,
+};
+
+@Component(StandaloneCompConfig)
+class StandaloneComponent {
+  control = new FormControl<string>('', [NbFormValidators.required(true)]);
+  errInfo = { [NbControlErrType.REQUIRED]: 'The field is required!' };
+
+  get textContent() {
+    return this.elementRef.nativeElement.textContent?.trim();
+  }
+
+  constructor(private elementRef: ElementRef<HTMLElement>) { }
+}
+
+@Component({
+  ...StandaloneCompConfig,
+  imports: [NbFormTestingModule],
+})
+class StandaloneComponentWithNgModule extends StandaloneComponent { }
--- projects/nb-form/src/lib/pipes/test/err-info.pipe.spec.ts
+++ projects/nb-form/src/lib/pipes/test/err-info.pipe.spec.ts
@@ -1,69 +1,109 @@
+import { Component, ElementRef } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { NbCommonTestingModule, NbValueTypeService } from '@bigbear713/nb-common';
 import { of } from 'rxjs';
 import { NbControlErrType } from '../../constants';
+import { INbControlErrInfo } from '../../models';
 import { NbErrInfoPipe } from '../err-info.pipe';
 
 describe('NbErrInfoPipe', () => {
-  let pipe: NbErrInfoPipe;
-  let valueType: NbValueTypeService;
+  describe('used in normal case', () => {
+    let pipe: NbErrInfoPipe;
+    let valueType: NbValueTypeService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NbCommonTestingModule]
-    })
-      .compileComponents();
-  });
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        imports: [NbCommonTestingModule]
+      })
+        .compileComponents();
+    });
 
-  beforeEach(() => {
-    pipe = new NbErrInfoPipe();
-    valueType = TestBed.inject(NbValueTypeService);
-  });
+    beforeEach(() => {
+      pipe = new NbErrInfoPipe();
+      valueType = TestBed.inject(NbValueTypeService);
+    });
 
-  it('create an instance', () => {
-    expect(pipe).toBeTruthy();
-  });
+    it('create an instance', () => {
+      expect(pipe).toBeTruthy();
+    });
 
-  describe('#transform()', () => {
-    [
-      {
-        title: 'the errors and errMapping all are undefined',
-        params: {
-          errors: undefined,
-          errMapping: undefined
+    describe('#transform()', () => {
+      [
+        {
+          title: 'the errors and errMapping all are undefined',
+          params: {
+            errors: undefined,
+            errMapping: undefined
+          },
+          expect: { isString: true, isObservable: false }
         },
-        expect: { isString: true, isObservable: false }
-      },
-      {
-        title: 'the errors has a required err and the errMapping is undefined',
-        params: {
-          errors: { [NbControlErrType.REQUIRED]: true },
-          errMapping: undefined
+        {
+          title: 'the errors has a required err and the errMapping is undefined',
+          params: {
+            errors: { [NbControlErrType.REQUIRED]: true },
+            errMapping: undefined
+          },
+          expect: { isString: true, isObservable: false }
         },
-        expect: { isString: true, isObservable: false }
-      },
-      {
-        title: 'the errors has a required err and the errMapping has a string value',
-        params: {
-          errors: { [NbControlErrType.REQUIRED]: true },
-          errMapping: { [NbControlErrType.REQUIRED]: 'This field is required!' }
+        {
+          title: 'the errors has a required err and the errMapping has a string value',
+          params: {
+            errors: { [NbControlErrType.REQUIRED]: true },
+            errMapping: { [NbControlErrType.REQUIRED]: 'This field is required!' }
+          },
+          expect: { isString: true, isObservable: false }
         },
-        expect: { isString: true, isObservable: false }
-      },
-      {
-        title: 'the errors has a required err and the errMapping has a observable value',
-        params: {
-          errors: { [NbControlErrType.REQUIRED]: true },
-          errMapping: { [NbControlErrType.REQUIRED]: of('This field is required!') }
-        },
-        expect: { isString: false, isObservable: true }
-      }
-    ].forEach(item => {
-      it(item.title, () => {
-        const result = pipe.transform(item.params.errors, item.params.errMapping);
-        expect(valueType.isString(result)).toEqual(item.expect.isString);
-        expect(valueType.isObservable(result)).toEqual(item.expect.isObservable);
+        {
+          title: 'the errors has a required err and the errMapping has a observable value',
+          params: {
+            errors: { [NbControlErrType.REQUIRED]: true },
+            errMapping: { [NbControlErrType.REQUIRED]: of('This field is required!') }
+          },
+          expect: { isString: false, isObservable: true }
+        }
+      ].forEach(item => {
+        it(item.title, () => {
+          const result = pipe.transform(item.params.errors, item.params.errMapping);
+          expect(valueType.isString(result)).toEqual(item.expect.isString);
+          expect(valueType.isObservable(result)).toEqual(item.expect.isObservable);
+        });
       });
     });
   });
+
+  describe('used in standalone component', () => {
+    [
+      {
+        title: 'imported by standalone component',
+        createComp: () => TestBed.createComponent(StandaloneComponent)
+      },
+    ].forEach(item => {
+      it(item.title, () => {
+        const fixture = item.createComp();
+        const component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        expect(component.textContent).toEqual('The field is required!');
+      });
+    })
+  });
+
 });
+
+const StandaloneCompConfig = {
+  standalone: true,
+  imports: [NbErrInfoPipe],
+  template: `{{errors|nbErrInfo:errInfo}}`,
+};
+
+@Component(StandaloneCompConfig)
+class StandaloneComponent {
+  errors = {[NbControlErrType.REQUIRED]:true};
+  errInfo: INbControlErrInfo = { [NbControlErrType.REQUIRED]:'The field is required!' };
+
+  get textContent() {
+    return this.elementRef.nativeElement.textContent?.trim();
+  }
+
+  constructor(private elementRef: ElementRef<HTMLDivElement>) { }
+}

ad05452068ea6d09b02e70ef13b4b01129342c72
bigBear713 <774326739@qq.com>
2024-06-23 13:58:25 +0800

    chore: upgrade to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,39 +5,33 @@ import { NbTransLang, NbTransModule, NbTransService, NB_TRANS_LOADER } from '@bi
 import { NbFormModule, NbControlErrType, NB_CONTROL_COMMON_ERR_INFO } from 'nb-form';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NbFormModule,
-    NbTransModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    {
-      provide: NB_CONTROL_COMMON_ERR_INFO,
-      useFactory: (transService: NbTransService) => ({
-        [NbControlErrType.FILE_TYPE]: transService.translationAsync('errors.fileType'),
-        [NbControlErrType.FILE_MIN_SIZE]: 'The file min size is 100KB!!!!',
-        [NbControlErrType.EQUAL]: '两个控件值不相等',
-      }),
-      deps: [NbTransService],
-    },
-    {
-      provide: NB_TRANS_LOADER,
-      useFactory: (http: HttpClient) => {
-        return {
-          [NbTransLang.ZH_CN]: () => http.get('./assets/localization/zh-CN/translations.json'),
-          [NbTransLang.EN]: () => http.get('./assets/localization/en/translations.json'),
-        };
-      },
-      deps: [HttpClient],
-    },
-  ],
-  bootstrap: [AppComponent],
-})
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NbFormModule,
+        NbTransModule,
+        AppRoutingModule], providers: [
+        {
+            provide: NB_CONTROL_COMMON_ERR_INFO,
+            useFactory: (transService: NbTransService) => ({
+                [NbControlErrType.FILE_TYPE]: transService.translationAsync('errors.fileType'),
+                [NbControlErrType.FILE_MIN_SIZE]: 'The file min size is 100KB!!!!',
+                [NbControlErrType.EQUAL]: '两个控件值不相等',
+            }),
+            deps: [NbTransService],
+        },
+        {
+            provide: NB_TRANS_LOADER,
+            useFactory: (http: HttpClient) => {
+                return {
+                    [NbTransLang.ZH_CN]: () => http.get('./assets/localization/zh-CN/translations.json'),
+                    [NbTransLang.EN]: () => http.get('./assets/localization/en/translations.json'),
+                };
+            },
+            deps: [HttpClient],
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
